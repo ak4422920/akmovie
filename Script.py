999476ba16d024ef0cb1fd5072dc4fbec675eda0
ak4422920 <ak4422920@gmail.com>
2025-02-11 21:04:31 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -518,7 +518,7 @@ Hey <a href='tg://settings'>ᴍʏ ғʀɪᴇɴᴅ</a>
 
  How and what to do
 
- Step 1: Administer this VJ-FILTER-BOT bot to your group
+ Step 1: Administer this bot to your group
 
  Step 2: Add your website and API
 
